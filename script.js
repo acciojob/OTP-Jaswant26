89a1f5ffc4c87816b142d7a5f67a45076cd9e50d
Jaswant Singh Rawat <71880220+Jaswant26@users.noreply.github.com>
2023-10-30 18:23:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function moveToNext(input, nextInputId) {
+            if (input.value.length === 1) {
+                document.getElementById(nextInputId).focus();
+            } else if (input.value.length === 0 && input.previousElementSibling) {
+                input.previousElementSibling.focus();
+            }
+        }
